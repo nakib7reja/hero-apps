4e72679ef91e163f5cdf9dd5ea4b579cc7297e38
NokibReja <nakib7reja@gmail.com>
2025-12-04 17:10:39 +0600

    home page added

--- src/Pages/Hom.jsx
+++ src/Pages/Hom.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { Link, useLoaderData } from 'react-router';
 import AppCard from '../components/AppCard/AppCard';
 
 const Hom = () => {
     const apps = useLoaderData()
+    const homeApps = apps.slice(0, 7)
     // console.log(apps)
     return (
-        <div className='m-5 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-5'>
-            {
-                apps.map(app =><AppCard key={app.id} app={app}></AppCard>)
-            }
-        </div>
+        <>
+            <div className="text-center my-10">
+                <h1 className='text-5xl font-bold m-5'>Trending Apps</h1>
+                <p>Explore All Trending Apps on the Market developed by us </p>
+            </div>
+            <div className='m-5 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-10 mx-auto'>
+                {
+                    homeApps.map(app => <AppCard key={app.id} app={app}></AppCard>)
+                }
+            </div>
+            <div className="text-center my-10">
+                {/* <a>See All</a> */}
+                <Link className='btn btn-primary' to='/apps' >See all Apps</Link>
+                {/* <button>See All</button> */}
+            </div>
+        </>
     );
 };
 
--- src/components/AppCard/AppCard.jsx
+++ src/components/AppCard/AppCard.jsx
@@ -3,26 +3,26 @@ import React from 'react';
 const AppCard = ({ app }) => {
     const { image, title, ratingAvg, downloads } = app
     return (
-        <div className="card bg-base-100 w shadow-sm">
-            <figure>
+        <div className="card bg-base-100 w-60 shadow-sm mx-auto hover:scale-110 transition ease-in-out">
+            <figure className='w-[80%] mx-auto'>
                 <img
-                    className='w-50 h-50 object-cover'
+                    className='w-50 h-50 object-contain'
                     src={image}
                     alt="AppsImg" />
             </figure>
-            <div className="card-body">
-                <h2 className="card-title">{title}</h2>
+            <div className="card- p-3 my-2">
+                <h2 className="card-title text-2xl mb-2">{title}</h2>
                 <div className="flex justify-around gap-10">
-                    <p className='flex gap-1 p-2 rounded-2xl text-center font-bold bg-green-100 text-green-500'>
-                        <img className='w-5 h-5' src="https://i.ibb.co.com/DgQ4yhRx/icon-downloads.png" alt="" />
+                    <p className='flex gap-1 py-1 px-2 rounded-2xl text-center font-bold bg-green-100 text-green-500'>
+                        <img className='w-4 h-4 mt-1 ' src="https://i.ibb.co.com/DgQ4yhRx/icon-downloads.png" alt="" />
                         {downloads}M</p>
-                    <p className='flex gap-1 p-2  rounded-2xl text-center font-bold bg-orange-100 text-orange-500'>
-                        <img className='w-5 h-5' src="https://i.ibb.co.com/B5xBqrV6/icon-ratings.png" alt="" />
+                    <p className='flex gap-1  py-1 px-2  rounded-2xl text-center font-bold bg-orange-100 text-orange-500'>
+                        <img className='w-4 h-4 mt-1 ' src="https://i.ibb.co.com/B5xBqrV6/icon-ratings.png" alt="" />
                         {ratingAvg}</p>
                 </div>
-                <div className="card-actions justify-end">
+                {/* <div className="card-actions justify-end">
                     <button className="btn btn-primary">Install</button>
-                </div>
+                </div> */}
             </div>
         </div>
     );
